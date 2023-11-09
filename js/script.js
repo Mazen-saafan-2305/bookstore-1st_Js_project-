d6a3501c6bb8406043edf0e0d9873dd5e3b2ec3d
mazen saafan <m.saf.222525@gmail.com>
2023-11-09 14:09:40 +0200

    create project

---
+++ js/script.js
@@ -0,0 +1,83 @@
+var content_items = " ";
+var prices =0;
+document.getElementById("product1_btn").onclick=function()
+{
+  var product_price = document.getElementById("price1").textContent;
+  var content=document.getElementById("bookname1").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
+document.getElementById("product2_btn").onclick=function()
+{
+  var product_price = document.getElementById("price2").textContent;
+  var content=document.getElementById("bookname2").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
+document.getElementById("product3_btn").onclick=function()
+{
+  var product_price = document.getElementById("price3").textContent;
+  var content=document.getElementById("bookname3").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
+document.getElementById("product4_btn").onclick=function()
+{
+  var product_price = document.getElementById("price4").textContent;
+  var content=document.getElementById("bookname4").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
+document.getElementById("product5_btn").onclick=function()
+{
+  var product_price = document.getElementById("price5").textContent;
+  var content=document.getElementById("bookname5").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
+document.getElementById("product6_btn").onclick=function()
+{
+  var product_price = document.getElementById("price6").textContent;
+  var content=document.getElementById("bookname6").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
+document.getElementById("product7_btn").onclick=function()
+{
+  var product_price = document.getElementById("price7").textContent;
+  var content=document.getElementById("bookname7").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
+document.getElementById("product8_btn").onclick=function()
+{
+  var product_price = document.getElementById("price8").textContent;
+  var content=document.getElementById("bookname8").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
+document.getElementById("product9_btn").onclick=function()
+{
+  var product_price = document.getElementById("price9").textContent;
+  var content=document.getElementById("bookname9").textContent;
+  content_items+=content+", ";
+  prices+=parseInt(product_price);
+  document.getElementById("total_price").innerHTML=" "+prices+" EGP";
+  document.getElementById("items").innerHTML=content_items;
+}
